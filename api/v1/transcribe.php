dccd731b6a222567716bec27dd38c332d3d9da6e
Tu TV <tutv95@gmail.com>
2015-04-23 01:05:00 +0700

    Xong transcribe

---
+++ api/v1/transcribe.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tu
+ * Date: 22/04/2015
+ * Time: 23:52
+ */
+header("Access-Control-Allow-Origin:*");
+header("Content-Type:application/json; charset=UTF-8");
+
+$texts = $_POST['t'];
+
+if (!isset($texts)) {
+    echoResult(false, null);
+}
+else {
+    include("Unirest.php");
+
+    $body = array("text_to_transcribe" => $texts, "submit" => "Show transcription",
+        "output_dialect" => "am", "output_style" => "only_tr", "preBracket" => "", "postBracket" => "");
+    $headers = null;
+    $content = Unirest\Request::post("http://lingorado.com/ipa", $headers, $body);
+    $raw = $content->raw_body;
+
+    $temp = explode("<p>", $raw)[1];
+    $temp = explode("</p>", $temp)[0];
+    $arr = explode("class=\"transcribed_word\">", $temp);
+    $trans = "";
+    for ($i = 1; $i < count($arr); $i++) {
+        $trans .= tachXau(explode("</span>", $arr[$i])[0]) . " ";
+    }
+
+    echoResult(true, $trans);
+}
+
+function tachXau($str) {
+    if (explode("<a", $str)[1] == null) {
+        return $str;
+    }
+
+    $temp = explode(">", $str)[1];
+    $temp = explode("<", $temp)[0];
+    return $temp;
+}
+
+function echoResult($s, $t) {
+    echo json_encode(array("status" => $s, "trans" => $t));
+}
+
+?>
